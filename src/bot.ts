c10ad6740f2398a3a3657d0d487612db2de648e2
Gameheld <lukas.liesenberg@web.de>
2024-05-20 19:16:38 +0200

    Add inline query functionality
    - retrieves command list that fit the query
    - assigns random uuid to each as required by telegram api
    - answers query with photo_url(s) inside the github repo
    - new GIT_LINK .env entry added

--- src/bot.ts
+++ src/bot.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto'
 import dotenv from 'dotenv'
 import TelegramBot from 'node-telegram-bot-api'
 import { Command } from './types.js'
@@ -17,4 +18,22 @@ bot.onText(commandListRegExp, async (msg) => {
   if (command !== undefined) {
     bot.sendPhoto(msg.chat.id, './img/' + command.fileName, { reply_to_message_id: msg.message_id })
   }
+})
+
+bot.on('inline_query', async (query) => {
+  const uuids: string[] = []
+  const matches: Command[] = commandList.filter((c) => c.name.toLowerCase().includes(query.query.toLowerCase()))
+
+  while (uuids.length !== matches.length) {
+    const uuid: string = crypto.randomUUID()
+
+    if (!uuids.includes(uuid)) {
+      uuids.push(uuid)
+    }
+  }
+
+  bot.answerInlineQuery(query.id, matches.map((c) => {
+    const link: string = (process.env.GIT_LINK ?? '') + '/blob/main/img/' + c.fileName + '?raw=true'
+    return { photo_url: link, thumb_url: link, id: uuids[matches.indexOf(c)], type: 'photo', description: c.info } as TelegramBot.InlineQueryResultPhoto
+  }))
 })
